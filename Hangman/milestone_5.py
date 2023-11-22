68988d99c86c30a4ed0d5063f4cc5b6251c4ec3f
rantirules <oa740@nyu.edu>
2023-11-22 12:19:24 +0000

    completed milestone 5

---
+++ Hangman/milestone_5.py
@@ -0,0 +1,48 @@
+import random
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word)) #  int - The number of UNIQUE letters in the word that have not been guessed yet
+        self.num_lives = 5 # int - The number of lives the player has at the start of the game.
+        self.word_list = [] # list - A list of words
+        self.list_of_guesses = []  #  list - A list of the guesses that have already been tried. Set this to an empty list initially
+    def check_guess(self, guess):
+        guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for w in self.word:
+                if w == guess:
+                    word_guessed[word_guessed.index(guess)] = guess
+            num_letters -= 1
+        else:
+            num_lives -= 1
+            print(f"Sorry, {guess} is not in word.")
+            print(f"You have {num_lives} left.")
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ")
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You've already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+    def play_game(self, word_list):
+        num_lives = 5
+        game = Hangman(word_list, num_lives)
+        while True:
+            if num_lives = 0:
+                print(f"You lost!")
+            elif num_letters > 0:
+                ask_for_input()
+            else:
+                print(f"Congratulations. You won the game!")
+            
+    play_game(['cherries', 'butterflies'])
+
+game = Hangman(['mangoes', 'banana'])
+game.ask_for_input()
